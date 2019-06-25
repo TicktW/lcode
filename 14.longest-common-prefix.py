857893b22dcbbc4bcb5593cc03844fa11c845139
TicktW <wxj06@qq.com>
2019-06-25 20:29:02 +0800

    9 is finished but not test

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=14 lang=python
+#
+# [14] Longest Common Prefix
+#
+# https://leetcode.com/problems/longest-common-prefix/description/
+#
+# algorithms
+# Easy (33.59%)
+# Total Accepted:    479.5K
+# Total Submissions: 1.4M
+# Testcase Example:  '["flower","flow","flight"]'
+#
+# Write a function to find the longest common prefix string amongst an array of
+# strings.
+# 
+# If there is no common prefix, return an empty string "".
+# 
+# Example 1:
+# 
+# 
+# Input: ["flower","flow","flight"]
+# Output: "fl"
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+# 
+# 
+# Note:
+# 
+# All given inputs are in lowercase letters a-z.
+# 
+#
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -19,29 +19,29 @@
 # 
 # Input: 121
 # Output: true
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: -121
 # Output: false
 # Explanation: From left to right, it reads -121. From right to left, it
 # becomes 121-. Therefore it is not a palindrome.
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: 10
 # Output: false
 # Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-# 
-# 
+#
+#
 # Follow up:
-# 
+#
 # Coud you solve it without converting the integer to a string?
-# 
+#
 #
 class Solution(object):
     def isPalindrome(self, x):
@@ -49,4 +49,19 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        
+        if x < 0:
+            return False
+
+        old_x = x
+        new_x = 0
+        while x > 0:
+            new_x = new_x*10 + x % 10
+            x = x // 10
+
+        print(new_x)
+        print(x)
+
+        return True if new_x == old_x else False
+
+# if __name__ == '__main__':
+#     print(Solution().isPalindrome(121))
