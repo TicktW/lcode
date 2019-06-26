af0c866f0332360cda1099ee1a189e19657523b2
TicktW <wxj06@qq.com>
2019-06-26 17:11:28 +0800

    passed 13

--- 13.roman-to-integer.py
+++ 13.roman-to-integer.py
@@ -13,8 +13,8 @@
 #
 # Roman numerals are represented by seven different symbols: I, V, X, L, C, D
 # and M.
-# 
-# 
+#
+#
 # Symbol       Value
 # I             1
 # V             5
@@ -82,5 +82,33 @@ class Solution(object):
         """
         :type s: str
         :rtype: int
+        Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
+
+        # I can be placed before V (5) and X (10) to make 4 and 9. 
+        # X can be placed before L (50) and C (100) to make 40 and 90. 
+        # C can be placed before D (500) and M (1000) to make 400 and 900.
         """
-        pass
+        before_dict = {
+            "I":1,
+        }
+        item_dict = \
+        {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        res = 0
+        for i in len(s)-1:
+            if s[i] in ("I", "X", "C")
+            res = item_dict[s[i]]
