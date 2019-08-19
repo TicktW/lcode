555e871c67e5e69f029f1967b89c28387f7ddfdf
TicktW <wxj06@qq.com>
2019-08-19 17:10:04 +0800

    add merge

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -33,33 +33,20 @@ class Solution(object):
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
+        l1 l2 l1.val > l2.val
         """
-        if l1 is None and l2 is None:
-            return l1
-        if l1 is None and l2 is not None:
+        if not l1:
             return l2
-        if l1 is not None and l2 is None:
+        if not l2:
             return l1
 
-        if l1.val < l2.val:
-            head_temp = l1
-            l1 = l1.next
+        if l1.val > l2.val:
+            head = l2
+            head.next = self.mergeTwoLists(l2.next, l1)
         else:
-            head_temp = l2
-            l2 = l2.next
+            head = l1
+            head.next = self.mergeTwoLists(l1.next, l2)
 
-        head = head_temp
-        while l1.next and l2.next:
-            if l1.val < l2.val:
-                head.next = l1
-                l1 = l1.next
-            else:
-                head.next = l2
-                l2 = l2.next
-            head = head.next
+        return head
 
-        if l1.next:
-            l2.next = l1.next
-        if l2.next:
-            l1.next = l2.next
-        return head_temp
+
