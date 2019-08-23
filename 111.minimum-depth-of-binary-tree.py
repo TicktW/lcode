c7ed536a8a40cafa053bc64d321821fd38f692f6
TicktW <wxj06@qq.com>
2019-08-23 15:01:44 +0800

    hit 111

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -33,11 +33,11 @@
 # 
 #
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def minDepth(self, root):
@@ -45,4 +45,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        if root is None:
+            return 0
+
+        if root.left is None and root.right is None:
+            return 1
+
+        if root.left is None and root.right is not None:
+            return 1 + self.minDepth(root.right)
+
+        if root.left is not None and root.right is None:
+            return 1 + self.minDepth(root.left)
+
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
