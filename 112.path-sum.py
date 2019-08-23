1c91d4537817a749757ded614b2392df6ac0ed9f
TicktW <wxj06@qq.com>
2019-08-23 15:10:07 +0800

    hit 112

--- 112.path-sum.py
+++ 112.path-sum.py
@@ -47,4 +47,10 @@ class Solution(object):
         :type sum: int
         :rtype: bool
         """
-        
+        if root is None:
+            return False
+
+        if sum == root.val and root.left is None and root.right is None:
+            return True
+
+        return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)  # logic is or; not and
