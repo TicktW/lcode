d0ae50746e0caed16715c0696c350c3de2bd7373
TicktW <wxj06@qq.com>
2019-08-23 14:41:43 +0800

    hit 110

--- 110.balanced-binary-tree.py
+++ 110.balanced-binary-tree.py
@@ -51,16 +51,29 @@
 # 
 #
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
+
+    def height(self, root):
+        if root is None:
+            return 0
+
+        return max(self.height(root.left), self.height(root.right)) + 1
+
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        
+        if root is None:
+            return True
+
+        if abs(self.height(root.left) - self.height(root.right)) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
